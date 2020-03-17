438ae297cb0e3f261bd33d56c1aa3f5ae4ca4ff3
WhiteHat\WhiteHat <johndonggyu@naver.com>
2020-03-17 16:59:38 +0900

    module.exports에 대한 주석

--- CodeStudy/commercial-paper/organization/magnetocorp/contract/index.js
+++ CodeStudy/commercial-paper/organization/magnetocorp/contract/index.js
@@ -14,5 +14,16 @@ const는 let과 마찬가지로 블록 레벨 스코프를 가진다.
 객체라면 객체에 대한 참조를 변경하지 못하고, 객체의 프로퍼티는 보호되지 않는다.
 https://poiemaweb.com/es6-block-scope 참고
 */
+/* written by KDG - require('./lib/papercontract.js');
+./lib/ 경로에 있는 papercontract.js 모듈을 불러온다.
+require 메서드는 내부적으로 module.exports를 반환한다.
+자세한건 https://dololak.tistory.com/97 참고하면 사용법 정도는 이해할 수 있음.
+*/
 const cpcontract = require('./lib/papercontract.js');
+/* written by KDG - module.exports.contracts
+index.js (현재파일)을 require함수로 가져가는 파일은 contracts라는 객체로
+./lib/papercontract.js 모듈을 사용할 수 있다.
+근데 index.js 파일을 require하는 부분은 찾아보니 없다.
+알아서 가져다 사용하라는 듯한데...?
+*/
 module.exports.contracts = [cpcontract];
